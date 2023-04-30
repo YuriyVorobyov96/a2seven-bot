bc3157da476881e90893f692f7cb4236c9855fdc
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-04-30 19:06:35 +0300

    added test commands

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import config from 'config';
 import { session, Telegraf } from 'telegraf';
 
+import ACommand from './commands/command.class';
+import StartCommand from './commands/start.command';
 import IContext from './common/interfaces/context.interface';
 
 const TELEGRAM_TOKEN: string = config.get('telegram.token');
@@ -8,12 +10,17 @@ const TELEGRAM_TOKEN: string = config.get('telegram.token');
 class Bot {
   private bot: Telegraf<IContext>;
 
+  private commands: ACommand[] = [];
+
   constructor(token: string) {
     this.bot = new Telegraf<IContext>(token);
     this.bot.use(session());
   }
 
   public init(): void {
+    this.commands = [new StartCommand(this.bot)];
+    this.commands.forEach((command) => command.handle());
+
     this.bot.launch();
   }
 }
